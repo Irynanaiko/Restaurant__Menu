cbfab065e4c8975261fe1795023da640e1d8bce3
Irynanaiko <irynaiko@ukr.net>
2021-09-22 09:12:00 +0300

    feat: add not found page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { MainComponent } from './components/main/main.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'admin', component: AdminComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
---
+++ client/src/app/components/admin/new-category/new-category.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Categories } from 'src/app/core/interfaces';
+
+@Component({
+  selector: 'app-new-category',
+  templateUrl: './new-category.component.html',
+  styleUrls: ['./new-category.component.scss'],
+})
+export class NewCategoryComponent implements OnInit {
+  newCategoryForm: FormGroup;
+  isSubmit = false;
+  modalHeader: string;
+  category: Categories;
+  constructor(private fb: FormBuilder, private bsmodalRef: BsModalRef) {}
+
+  ngOnInit(): void {
+    this.createNewCategoryForm();
+  }
+
+  private createNewCategoryForm(): void {
+    this.newCategoryForm = this.fb.group({
+      name: [
+        this.category?.name || null,
+        // [Validators.required, Validators.minLength(3)],
+      ],
+    });
+  }
+
+  onSubmit(): void {
+    this.isSubmit = true;
+
+    if (this.newCategoryForm.invalid) {
+      return;
+    }
+
+    this.isSubmit = false;
+
+    this.save(this.newCategoryForm.value);
+    this.modalHide();
+  }
+
+  modalHide(): void {
+    this.bsmodalRef.hide();
+  }
+
+  private save(newCategory: Categories): void {}
+}
