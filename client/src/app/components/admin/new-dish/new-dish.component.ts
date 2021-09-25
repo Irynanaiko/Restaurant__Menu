cb2dd57293856904e854fc822f2bceab4692d3f5
Irynanaiko <irynaiko@ukr.net>
2021-09-25 18:35:42 +0300

    refactor: apply validation to new dish form

--- client/src/app/components/admin/new-dish/new-dish.component.ts
+++ client/src/app/components/admin/new-dish/new-dish.component.ts
@@ -27,6 +27,18 @@ export class NewDishComponent implements OnInit {
   get nameControl(): any {
     return this.newDishForm.get('name') as FormControl;
   }
+  get imgControl(): any {
+    return this.newDishForm.get('img') as FormControl;
+  }
+  get priceControl(): any {
+    return this.newDishForm.get('price') as FormControl;
+  }
+  get weightControl(): any {
+    return this.newDishForm.get('weight') as FormControl;
+  }
+  get categoryIdControl(): any {
+    return this.newDishForm.get('categoryId') as FormControl;
+  }
 
   private createNewDishForm(): void {
     if (this.dish?.id) {
@@ -37,11 +49,12 @@ export class NewDishComponent implements OnInit {
         ],
 
         id: [this.dish.id || null],
-        img: [this.dish?.img || null],
-        price: [this.dish?.price || null],
-        weight: [this.dish?.weight || null],
-        descr: [this.dish?.descr || null],
-        categoryId: [this.dish?.categoryId || null],
+        img: [this.dish?.img || null, [Validators.required]],
+
+        price: [this.dish?.price || null, [Validators.required]],
+        weight: [this.dish?.weight || null, [Validators.required]],
+        descr: [this.dish?.descr || null, [Validators.required]],
+        categoryId: [this.dish?.categoryId || null, [Validators.required]],
       });
     } else {
       this.newDishForm = this.fb.group({
@@ -49,11 +62,12 @@ export class NewDishComponent implements OnInit {
           this.dish?.name || null,
           [Validators.required, Validators.minLength(3)],
         ],
-        img: [this.dish?.img || null],
-        price: [this.dish?.price || null],
-        weight: [this.dish?.weight || null],
-        descr: [this.dish?.descr || null],
-        categoryId: [this.dish?.categoryId || null],
+        img: [this.dish?.img || null, [Validators.required]],
+
+        price: [this.dish?.price || null, [Validators.required]],
+        weight: [this.dish?.weight || null, [Validators.required]],
+        descr: [this.dish?.descr || null, [Validators.required]],
+        categoryId: [this.dish?.categoryId || null, [Validators.required]],
       });
     }
   }
